8694f789d7b7ca976e534899e3d710d3549a9d49
YamasakiYumiko <yamasaki@jtc-hq.com>
2016-10-11 22:39:39 +0900

    Add files via upload

--- InventoryControlSystem/cakephp/app/Model/PaginateOrigin.php
+++ InventoryControlSystem/cakephp/app/Model/PaginateOrigin.php
@@ -62,23 +62,110 @@ class PaginateOrigin extends AppModel {
 		return $this->query($sql);
 	}
 
+// 	/**
+// 	 * ページネート実行（カウント処理）
+// 	 *
+// 	 */
+// 	public function paginateCount() {
+// 		$extra = func_get_arg(2);
+
+// 		return count($this->query(
+// 				preg_replace(
+// 						'/LIMIT \d+ OFFSET \d+$/u',
+// 						'',
+// 						$extra['extra']['type']
+// 						)
+// 				));
+// 	}
+
 	/**
 	 * ページネート実行（カウント処理）
 	 *
 	 */
 	public function paginateCount() {
 		$extra = func_get_arg(2);
-		return count($this->query(
-				preg_replace(
-						'/LIMIT \d+ OFFSET \d+$/u',
-						'',
-						$extra['extra']['type']
-						)
-				));
+
+
+
+
+		$strSql = <<< EOM
+  SELECT
+    COUNT(*)
+  FROM tbl_zks_obic_order as o
+    INNER JOIN tbl_zks_obic_order_detail AS od ON
+    od.order_slip_no = o.order_slip_no
+    LEFT JOIN mst_shop_for_tool AS ms ON
+    ms.dept_cd = o.warehouse_cd
+    LEFT JOIN (
+    		SELECT
+    		m1.staff_cd,
+    		m1.staff_name
+    		FROM (
+    				SELECT
+    				staff_cd,
+    				MAX(revision_date) AS revision_date
+    				FROM mst_obic_staff
+    				GROUP BY staff_cd
+    				) AS m0
+    		INNER JOIN mst_obic_staff AS m1 ON
+    		m1.staff_cd = m0.staff_cd AND
+    		m1.revision_date = m0.revision_date
+    		) AS mst ON
+    		mst.staff_cd = o.supplier_staff_cd
+    		LEFT JOIN mst_obic_supplier AS msp ON
+    		msp.supplier_cd = o.supplier_cd
+    		LEFT JOIN mst_obic_payee AS mpy ON
+    		mpy.payee_cd = o.payee_cd
+    		LEFT JOIN mst_obic_payment AS mpm ON
+    		mpm.payment_cd = o.payment_cd
+    		LEFT JOIN mst_obic_hauler AS mh ON
+    		mh.hauler_cd = o.hauler_cd
+    		LEFT JOIN mst_obic_item AS mi ON
+    		mi.item_cd = od.item_cd
+	WHERE mi.item_cd LIKE 'D%'
+	AND o.order_ymd >= '2016-10-06'
+	AND od.cancel_flg = '0'
+
+
+EOM;
+
+		$extra['extra']['type'] = $strSql;
+
+
+
+		$strArray=$this->query($extra['extra']['type']);
+
+
+// 		$count = $this->query($extra['extra']['type'])[0][0]['count'];
+
+		$count =$strArray[0][0]['count'];
+
+
+
+
+		return $count;
+
+
+
 	}
+
+
+
+
+
+
+
 
 	public function hasField($name, $checkVirtual = false) {
 		return true;
 	}
+
+
+
+
+
+
+
+
 
 }
